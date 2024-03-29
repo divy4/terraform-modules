501a25adf38cdf98bff28b2814009bf67b738ca5
Dan Ivy <danivy4@gmail.com>
2024-03-29 18:14:52 -0500

    Move state to s3

--- modules/prd/versions.tf
+++ modules/prd/versions.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.7"
 
+  backend "s3" {
+    bucket = "com-danivy-terraform-state"
+    key    = "prd.tfstate"
+    region = "us-east-2"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
